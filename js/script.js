69f59dce8a577a8fff54b684cd72f97617f32eea
Eric Snell <eric_snell21@yahoo.com>
2017-03-08 12:39:58 -0500

    very basic styling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 (() => {
 
-  window.addEventListener('keydown', (event) => {
-    console.log(event.keyCode)
+  window.addEventListener('keydown', (e) => {
 
-    const audio = document.querySelector('audio')
+    const audio = document.querySelector(`audio[data-key="${ e.keyCode }"]`)
+    const key = document.querySelector(`.key[data-key="${ e.keyCode }"]`)
+
+    if (!audio) return      // Stop the function from running altogether
+
+    audio.currentTime = 0   // Starts audio at the beginning
+
+    audio.play()
+
+    console.log(key)
+
   })
 
 })()
