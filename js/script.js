8b8a92bfa364f588dea4ed4830658c49befab6cb
Eric Snell <eric_snell21@yahoo.com>
2017-03-08 15:14:12 -0500

    fixed error: only turn loops siblings off

--- js/script.js
+++ js/script.js
@@ -7,26 +7,7 @@
 
     if (!audio) return      // Stop the function from running altogether
 
-
-
-
-
-    if (audio.hasAttribute('data-loop')) {
-
-      const loops = document.querySelectorAll('audio[data-loop="on"]')
-
-      audio.setAttribute('data-loop', 'on')
-
-      if (loops) {
-        for (let i = 0; i < loops.length; i++) {
-          loops[i].setAttribute('data-loop', 'off')
-          loops[i].pause()
-          console.log(loops[i])
-        }
-      }
-
-    }
-
+    if (audio.hasAttribute('data-loop')) stopLoops(audio)
 
     audio.currentTime = 0   // Starts audio at the beginning
 
@@ -35,6 +16,20 @@
     key.classList.add('active')  // Adds .active class
   }
 
+
+  // Function to only allow one loop to play at a time
+  function stopLoops(el) {
+    const loops = document.querySelectorAll('audio[data-loop="on"]')
+
+    el.setAttribute('data-loop', 'on')
+
+    for (let i = 0; i < loops.length; i++) {
+      if (loops[i] !== el) {
+        loops[i].setAttribute('data-loop', 'off')
+        loops[i].pause()
+      }
+    }
+  }
 
 
   // Function that removes active class
