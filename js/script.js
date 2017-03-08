4c25a4b69d03ac338fec6a4e050035c4159a3bb0
Eric Snell <eric_snell21@yahoo.com>
2017-03-08 14:55:45 -0500

    function to allow only one audio loop to play at a time

--- js/script.js
+++ js/script.js
@@ -7,12 +7,35 @@
 
     if (!audio) return      // Stop the function from running altogether
 
+
+
+
+
+    if (audio.hasAttribute('data-loop')) {
+
+      const loops = document.querySelectorAll('audio[data-loop="on"]')
+
+      audio.setAttribute('data-loop', 'on')
+
+      if (loops) {
+        for (let i = 0; i < loops.length; i++) {
+          loops[i].setAttribute('data-loop', 'off')
+          loops[i].pause()
+          console.log(loops[i])
+        }
+      }
+
+    }
+
+
     audio.currentTime = 0   // Starts audio at the beginning
 
     audio.play()            // Plays sound
 
     key.classList.add('active')  // Adds .active class
   }
+
+
 
   // Function that removes active class
   function removeTransition(e) {
